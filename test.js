108132aae28bf39eeac8cde24c1518426fb9282c
hyrofumi <leohrsagittarius+bitbucket@gmail.com>
2015-10-06 18:30:42 +0900

    add sourt function

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-function() {
-	var test = 0;
+var sortNumber = function(){
+	number.sort(function(a, b){
+		if( a == b ){ return 0;  }
+		return a < b ? -1 : 1;
+	});
 }
 
 var number = {19, 3, 81, 1, 24, 21};
-console.log(number);
+sortNumber( number );
+console.log( number );
